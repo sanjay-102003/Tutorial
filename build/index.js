90a94d10f1cee6c4189905606ba111e6ddc35b5d
Sanjay <s.sanjay@visailabs.com>
2023-07-24 18:01:15 +0530

    Revert "changes done"
    This reverts commit e647e24e1b3606c25acb1768621889500316991c.
    
    q
    X
    X

--- build/index.js
+++ build/index.js
@@ -1,12 +1,2 @@
 "use strict";
 console.log("Hello World!!");
-console.log("Hello");
-let a = 0;
-for (let i = 0; i < 10; i++) {
-  a += 1;
-  console.log("Hello");
-}
-
-a = { firstName: "sanjay" };
-a.lastName = "s";
-console.log(a);
