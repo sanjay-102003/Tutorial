f93a89582b9988436e8ac107d1082ff5abc051ec
Sanjay <s.sanjay@visailabs.com>
2023-07-26 13:30:40 +0530

    updated

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
 "use strict";
 console.log("Hello World!!");
+console.log("Hello");
+console.log("Hello");
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let person1 = {
+    name: "sanj",
+    age: "hss",
+};
+let person2 = new Person("Sanja");
+console.log(person1, person2);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 console.log("Hello World!!");
 console.log("Hello");
 console.log("Hello");
+
+interface person {
+  name: string;
+  age: string;
+}
+
+class Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+let person1: person = {
+  name: "sanj",
+  age: "hss",
+};
+
+let person2: Person = new Person("Sanja");
+
+console.log(person1, person2);
